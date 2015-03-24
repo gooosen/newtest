1b85f777e9e70091908c7a5706c8cca3f369141c
gooosen <gooosen@gmail.com>
2015-03-24 10:34:39 +0800

    init

---
+++ 一体化工具/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace 一体化工具
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.textBox1.Location = new System.Drawing.Point(6, 18);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(370, 504);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(382, 535);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "操作结果";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(411, 56);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(351, 138);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "文件操作";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 537);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(787, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(625, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "打开文档";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(411, 200);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(351, 87);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "更改存货分类";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "SKU统计";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(187, 34);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(155, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "说明：根据条码计算出SKU数";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(16, 72);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "去重复值";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "说明：把条码中重复项过渡掉";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(97, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "存货分类码";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(168, 24);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(173, 21);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(16, 24);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "执行";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(0, 17);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(151, 29);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(30, 21);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(104, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "鞋码位";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.button5);
+            this.groupBox4.Location = new System.Drawing.Point(411, 293);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(351, 58);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "价格分析";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(16, 20);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "计算";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 559);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "、";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ 一体化工具/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 一体化工具
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string src = null;
+        private ArrayList getFile()
+        {
+            StreamReader objReader = new StreamReader(src);
+            string sLine = "";
+            ArrayList fileContext = new ArrayList();
+            while (sLine != null)
+            {
+                sLine = objReader.ReadLine();
+                if (sLine != null && !sLine.Equals(""))
+                {
+                    string[] str = sLine.Split(',');
+                    ArrayList line = new ArrayList();
+                    line.Add(str[0]);
+                    line.Add(str[1]);
+                    fileContext.Add(line);
+                }
+            }
+            objReader.Close();
+            return fileContext;
+        }
+        private ArrayList getNo(ArrayList fileContext)
+        {
+            conn = new SqlConnection();
+            cmd = conn.CreateCommand();
+            conn.ConnectionString = connStr;
+            conn.Open();
+            ArrayList prices = new ArrayList();
+            for (int i = 0; i < fileContext.Count; i++)
+            {
+                ArrayList temp = (ArrayList)fileContext[i];
+                string no = temp[0].ToString();
+                string sql = "SELECT b.id from AA_Inventory as a ,AA_InventoryPrice as b where a.id = b.idinventory and a.code like '%" + no + "%'";
+                cmd = new SqlCommand(sql, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    ArrayList price = new ArrayList();
+                    price.Add(reader["id"].ToString());
+                    price.Add(temp[1]);
+                    price.Add(temp[0]);
+                    prices.Add(price);
+                }
+                reader.Close();
+            }
+            conn.Close();
+            return prices;
+        }
+
+
+        private ArrayList getNoName(ArrayList fileContext)
+        {
+            conn = new SqlConnection();
+            cmd = conn.CreateCommand();
+            conn.ConnectionString = connStr;
+            conn.Open();
+            ArrayList prices = new ArrayList();
+            for (int i = 0; i < fileContext.Count; i++)
+            {
+                ArrayList temp = (ArrayList)fileContext[i];
+                string no = temp[0].ToString();
+                string sql = "SELECT code from AA_Inventory  where code like '%" + no + "%'";
+                cmd = new SqlCommand(sql, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    ArrayList price = new ArrayList();
+                    price.Add(reader["code"].ToString());
+                    price.Add(temp[1]);
+                    price.Add(temp[0]);
+                    prices.Add(price);
+                }
+                reader.Close();
+            }
+            conn.Close();
+            return prices;
+        }
+        private SqlConnection conn;
+        private SqlCommand cmd;
+        private const string connStr = "server=192.168.8.4;uid=sa;pwd=!@#$wass6677;database=UFTData292713_000019";
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            openFileDialog1.Filter = "导入文件(*.txt)|*.txt";
+            openFileDialog1.ShowDialog();
+            src = openFileDialog1.FileName;
+            toolStripStatusLabel1.Text = "已打开文件："+src;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (src != null)
+            {
+
+                textBox1.Text = "";
+                StreamReader objReader = new StreamReader(src);
+                string sLine = "";
+                ArrayList SKUcode = new ArrayList();
+                string temp = "";
+                string line = objReader.ReadLine();
+                string nul = textBox3.Text;
+                int nl = 0;
+                bool isStart = true;
+                try
+                {
+                    nl = int.Parse(nul);
+                }
+                catch (Exception ex)
+                {
+
+                }
+                int a =0;
+                string str1 = "";
+                string str2 = "";
+                while (sLine != null)
+                {
+                    if (isStart)
+                    {
+                        str1 = line.Substring(0, line.Length - nl);
+                        str2 = line.Substring(line.Length - nl, nl);
+                        line = str1+"," + str2;
+                        isStart = false;
+                        continue;
+                    }
+                    sLine = objReader.ReadLine();
+                    if (sLine != null && !sLine.Equals(""))
+                    {
+                        str1 = sLine.Substring(0, sLine.Length - nl);
+                        str2 = sLine.Substring(sLine.Length - nl, nl);
+
+                        if (temp.Equals(str1))
+                        {
+
+                            line += "," + str2;
+
+                        }
+                        else
+                        {                           
+                            string[] s = line.Split(',');
+                            a = s.Length - 1;
+                            line += ",共计" + a.ToString() + "双";
+                            textBox1.Text += line;
+                            line = "\r\n" + str1 + "," + str2;
+                        }
+
+                        temp = str1;
+                        toolStripStatusLabel2.Text = "正在分析";
+                    }
+                }
+
+                string[] sa = line.Split(',');
+                a = sa.Length - 1;
+                line += ",共计" + a.ToString() + "双";
+                textBox1.Text += line;
+                line = "\r\n" + str1 + "," + str2;
+                objReader.Close();
+                toolStripStatusLabel2.Text = "操作完成";
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (src != null)
+            {
+                textBox1.Text = "";
+                StreamReader objReader = new StreamReader(src);
+                string sLine = "";
+                ArrayList SKUcode = new ArrayList();
+                string sLine1 = objReader.ReadLine();
+                while (sLine != null)
+                {
+
+
+                    sLine = objReader.ReadLine();
+                    try
+                    {
+                        if (sLine.Equals(sLine1))
+                        {
+                            continue;
+                        }
+
+
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                    textBox1.Text += sLine + "\r\n";
+                    sLine1 = sLine;
+
+                }
+                objReader.Close();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (src != null)
+            {
+                textBox1.Text = "";
+                StreamReader objReader = new StreamReader(src);
+                string sLine = "";
+                ArrayList SKUcode = new ArrayList();
+                int n = 0;
+                while (sLine != null)
+                {
+                    sLine = objReader.ReadLine();
+                    if (sLine != null && !sLine.Equals(""))
+                    {
+                        conn = new SqlConnection();
+                        cmd = conn.CreateCommand();
+                        conn.ConnectionString = connStr;
+                        conn.Open();
+                        string sql1 = "select id from AA_InventoryClass where code like '" + textBox2.Text + "'";
+                        cmd = new SqlCommand(sql1, conn);
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        string id = "";
+                        while (reader.Read())
+                        {
+                            id = reader["id"].ToString();
+                        }
+                        reader.Close();
+
+                        string sql = "update AA_Inventory set idinventoryclass = '" + id + "' WHERE code like '" + sLine + "'";
+
+                        cmd = new SqlCommand(sql, conn);
+                        int i = cmd.ExecuteNonQuery();
+                        conn.Close();
+
+                    }
+                }
+                objReader.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A && e.Control)
+            {
+                textBox1.SelectAll();
+            }   
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            if (src != null)
+            {
+                ArrayList temp = getFile();
+                ArrayList temp2 = getNoName(temp);
+                for (int i = 0; i < temp2.Count; i++)
+                {
+                    ArrayList temp3 = (ArrayList)temp2[i];
+                    textBox1.Text += temp3[0] +","+temp3[1]+ "\r\n";
+                }
+            }
+            toolStripStatusLabel2.Text = "操作完成";
+        }
+    }
+}
